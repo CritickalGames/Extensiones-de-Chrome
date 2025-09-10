a9af6423129255ce77c6b84d06da95ab0ca3dc19
Zgtale-pc <zgtale1@hotmail.com>
2025-09-10 04:01:00 -0300

    versiones

---
+++ anime_tracker/docs/versiones.js
@@ -0,0 +1,18 @@
+/*
+!v0.2.4
+todo: FIX/mostrar los datos de los animes guardados en formato de carpetas
+todo: ADD/funcionalidad para marcar capítulos como vistos y actualizar el estado del anime
+!v0.3.0
+todo: ADD/nuevas tablas en la base de datos para gestionar las carpetas
+!v0.4.0
+todo: ADD/buscar anime en la api o db y mostrar los resultados en una tabla
+todo: ADD/elegir un anime buscado y guardarlo en la base de datos
+!v0.4.1
+todo: ADD/cargar una foto de portada manualmente si no se encuentra en la api
+!v0.5.0
+todo: FIX/un anime guardado y cargar la página de ese anime al hacer click en la portada
+!v0.5.1
+todo: FIX/guardar el capitulo como "ver" automaticamente si se encuentra el anime en la DB
+!v0.6.0
+todo: FIX/ESTILO
+*/
