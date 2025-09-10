c83395120c9d7cd6a6e62db4a4264987c6696cf2
Zgtale-pc <zgtale1@hotmail.com>
2025-09-10 04:29:39 -0300

    versiones

--- anime_tracker/docs/versiones.js
+++ anime_tracker/docs/versiones.js
@@ -16,3 +16,40 @@ todo: FIX/guardar el capitulo como "ver" automaticamente si se encuentra el anim
 !v0.6.0
 todo: FIX/ESTILO
 */
+
+// TODO: v0.2.4
+/*
+Creo que debo cambiar los datos que guardo en la base de datos.
+//* La clave primaria se mantiene
+//* Las propiedades que se mantienen son:
+-nombre
+-url_anime
+-url_dir
+-portada
+-capitulo
+//* base de datos segundarias
+-emision: FK:anime; emision (emitiendose, finalizado, pelicula)
+-visto: FK:anime; visto (viendo, completado, pendiente);
+-tags: FK:anime; tipo (serie, pelicula, ova, etc);
+-temporada: FK:anime; temporada (primavera, verano, etc); año;
+-genero: FK:anime; genero (accion, comedia, etc);
+-relación: FK:anime; relacion (secuela, precuela, etc);
+-actualización: FK:anime; día (lunes, martes, etc);
+-nota: FK:anime; nota (1-10);
+-favorito: FK:anime; favorito (si/no);
+-fecha_ultimo_visto: FK:anime; fecha (fecha de agregado);
+-idioma: FK:anime; doblaje (japones, español, etc); subtitulo (español, inglés, etc);
+//* Las carpetas se deben separar en:
+-"todos"
+---
+-"viendo"
+-"completados"
+-"pendientes"
+---
+-"emision"
+-"finalizados"
+---
+-"películas"
+-"mangas"
+-tags propias
+*/
